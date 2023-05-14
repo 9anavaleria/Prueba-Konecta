69095e58dd28654c36ff8ab8604ec49d5f52a0c8
9anavaleria <95309779+9anavaleria@users.noreply.github.com>
2023-05-14 12:17:18 -0500

    Update Consulta a base de datos.sql

--- Consulta a base de datos.sql
+++ Consulta a base de datos.sql
@@ -1,12 +1,12 @@
 Consulta Base de datos
 
-- Realizar una consulta que permita conocer cuál es el producto que más stock tiene.
+#Realizar una consulta que permita conocer cuál es el producto que más stock tiene.
     Opcion 1
          SELECT * FROM producto where `stock` = (SELECT MAX(`stock`) FROM producto);
     Opcion 2
         SELECT * FROM producto ORDER BY `stock` DESC limit 1;
 
-- Realizar una consulta que permita conocer cuál es el producto más vendido.
+#Realizar una consulta que permita conocer cuál es el producto más vendido.
     SELECT  producto.id_producto AS ID, 
             producto.nombre AS Nombre, 
             producto.referencia AS Refrerencia, 
