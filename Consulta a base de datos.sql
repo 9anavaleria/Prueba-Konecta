99955368a82cfd7fb86773b524faa290736d8581
9anavaleria <95309779+9anavaleria@users.noreply.github.com>
2023-05-14 12:07:36 -0500

    Consultas

---
+++ Consulta a base de datos.sql
@@ -0,0 +1,21 @@
+Consulta Base de datos
+
+- Realizar una consulta que permita conocer cuál es el producto que más stock tiene.
+    Opcion 1
+         SELECT * FROM producto where `stock` = (SELECT MAX(`stock`) FROM producto);
+    Opcion 2
+        SELECT * FROM producto ORDER BY `stock` DESC limit 1;
+
+- Realizar una consulta que permita conocer cuál es el producto más vendido.
+    SELECT  producto.id_producto AS ID, 
+            producto.nombre AS Nombre, 
+            producto.referencia AS Refrerencia, 
+            producto.precio AS Precio, 
+            producto.peso AS Precio, 
+            producto.categoria AS Categoria, 
+            producto.stock AS Stock, 
+            SUM(venta.cant_venta) AS Ventas 
+    FROM producto join venta ON producto.id_producto = venta.id_producto 
+    GROUP BY producto.nombre 
+    ORDER BY Ventas DESC 
+    LIMIT 1;
